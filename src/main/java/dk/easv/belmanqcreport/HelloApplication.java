c722c9e5ff25d7564c943bdfa09a324337839030
NadjaBrandenstein <nadbra01@easv365.dk>
2025-04-25 11:15:34 +0200

    ..

--- src/main/java/dk/easv/belmanqcreport/HelloApplication.java
+++ src/main/java/dk/easv/belmanqcreport/HelloApplication.java
@@ -2,7 +2,9 @@ package dk.easv.belmanqcreport;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Operator.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
+        Scene scene = new Scene(fxmlLoader.load(), screenBounds.getWidth(), screenBounds.getHeight());
         stage.setTitle("Belman");
         stage.setScene(scene);
         stage.show();
