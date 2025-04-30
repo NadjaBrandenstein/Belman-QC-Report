f4cd628b42343944d153833ac1a5adfc27761a7f
Kimphung3105 <kimngu01@easv365.dk>
2025-04-30 10:33:41 +0200

    Implement logic to store the PDF

--- src/main/java/dk/easv/belmanqcreport/GUI/Controller/QcController.java
+++ src/main/java/dk/easv/belmanqcreport/GUI/Controller/QcController.java
@@ -1,6 +1,8 @@
 package dk.easv.belmanqcreport.GUI.Controller;
 // JavaFX Imports
 import javafx.event.ActionEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
@@ -12,6 +14,8 @@ import java.io.IOException;
 
 public class QcController {
 
+
+    private Window primaryStage;
 
     public void btnBack(ActionEvent actionEvent) {
     }
@@ -32,28 +36,36 @@ public class QcController {
     }
 
     public void btnPDFSave(ActionEvent actionEvent) {
-        File file = new File("GeneratedReport.pdf");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save PDF");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF files", "*.pdf"));
+        fileChooser.setInitialFileName("Report.pdf");
+        
+        File file = fileChooser.showSaveDialog(primaryStage);
+        
+        if (file != null) {
+            try (PDDocument document = new PDDocument()) {
+                PDPage page = new PDPage(PDRectangle.A4);
+                document.addPage(page);
 
-        try (PDDocument document = new PDDocument()) {
-            PDPage page = new PDPage(PDRectangle.A4);
-            document.addPage(page);
+
+                PDPageContentStream contentStream = new PDPageContentStream(document, page);
+                contentStream.beginText();
+                contentStream.setFont(PDType1Font.HELVETICA_BOLD, 18);
 
 
-            PDPageContentStream contentStream = new PDPageContentStream(document, page);
-            contentStream.beginText();
-            contentStream.setFont(PDType1Font.HELVETICA_BOLD, 18);
+                contentStream.newLineAtOffset(100, 750);
 
+                contentStream.showText("This is a sample PDF report.");
+                contentStream.endText();
+                contentStream.close();
 
-            contentStream.newLineAtOffset(100, 750);
-
-            contentStream.showText("This is a sample PDF report.");
-            contentStream.endText();
-            contentStream.close();
-
-            document.save(file);
-            System.out.println("PDF saved to " + file.getAbsolutePath());
-        } catch (IOException e) {
-            e.printStackTrace();
+                document.save(file);
+                System.out.println("PDF saved to " + file.getAbsolutePath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        
     }
 }
