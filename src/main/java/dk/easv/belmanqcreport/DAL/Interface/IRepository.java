3ea688494edbe8d6b5ef6c9d46061f760ae4492b
Svoldgaard <Svoldgaard96@gmail.com>
2025-05-13 21:24:08 +0200

    order change to OrderRepository

--- src/main/java/dk/easv/belmanqcreport/DAL/Interface/IRepository.java
+++ src/main/java/dk/easv/belmanqcreport/DAL/Interface/IRepository.java
@@ -1,6 +1,7 @@
 package dk.easv.belmanqcreport.DAL.Interface;
 
 import dk.easv.belmanqcreport.BE.MyImage;
+import dk.easv.belmanqcreport.BE.Order;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -11,4 +12,5 @@ public interface IRepository<T> {
     T add(T item) throws Exception;
     T update(T item) throws Exception;
     void delete(T item) throws Exception;
+
 }
